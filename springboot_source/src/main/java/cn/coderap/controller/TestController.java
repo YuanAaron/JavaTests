ac4c2defd437803bc144167edea88c44dd705a35
oshacker <xjtu_yuanwei@163.com>
2022-04-04 12:45:55 +0800

    自定义初始化器一

---
+++ springboot_source/src/main/java/cn/coderap/controller/TestController.java
@@ -0,0 +1,18 @@
+package cn.coderap.controller;
+
+import cn.coderap.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    @RequestMapping("/test1")
+    public String test1() {
+        return testService.test1();
+    }
+}
