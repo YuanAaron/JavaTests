c5e25b20977b852934d7a8f90eecd2b4cd6c4985
oshacker <xjtu_yuanwei@163.com>
2022-04-18 16:06:15 +0800

    改进：异步发布事件

--- spring_source/src/main/java/cn/coderap/listener/MyServiceConfig.java
+++ spring_source/src/main/java/cn/coderap/listener/MyServiceConfig.java
@@ -1,8 +1,11 @@
 package cn.coderap.listener;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class MyServiceConfig {
@@ -11,6 +14,25 @@ public class MyServiceConfig {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyServiceConfig.class);
         context.getBean(MyService.class).register();
         context.close();
+    }
+
+    // 配置有线程池的SimpleApplicationEventMulticaster类型的bean会覆盖掉正常情况下未配置线程迟的该bean
+    @Bean
+    public SimpleApplicationEventMulticaster applicationEventMulticaster(@Qualifier("taskExecutor") ThreadPoolTaskExecutor taskExecutor) {
+        SimpleApplicationEventMulticaster multicaster = new SimpleApplicationEventMulticaster();
+        // 配置线程池
+        multicaster.setTaskExecutor(taskExecutor);
+        return multicaster;
+    }
+
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        return executor;
     }
 
     @Bean
